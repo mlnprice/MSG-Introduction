f901a1c7b5d9a48c6191a76399638f165c5b034b
mlnprice <47993136+mlnprice@users.noreply.github.com>
2019-05-13 12:52:10 -0600

    Update test_my_pkg.py

--- tests/test_my_pkg.py
+++ tests/test_my_pkg.py
@@ -1,4 +1,4 @@
-"""Tests the mathematical functions defined in my_pkg/trial.py
+"""Tests the mathematical functions defined in my_pkg/trail.py
 """
 
 import pytest
@@ -9,16 +9,3 @@ def test_square():
     from my_pkg.trial import square
 
     assert 4 == square(2)
-
-def test_factorial():
-    """Tests the factorial function."""
-
-    from my_pkg.trial import factorial
-
-    assert 24 == factorial(4)
-    assert 6 == factorial(3.0)
-    assert 1 == factorial(0)
-    assert 1 == factorial(-1)
-
-    with pytest.raises(ValueError):
-        factorial(3.5)
