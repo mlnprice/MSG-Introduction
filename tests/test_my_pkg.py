07c278fd2c97153c76fe710fade421dbad20e5e0
mlnprice <47993136+mlnprice@users.noreply.github.com>
2019-05-13 13:12:07 -0600

    Update test_my_pkg.py

--- tests/test_my_pkg.py
+++ tests/test_my_pkg.py
@@ -9,6 +9,9 @@ def test_square():
     from my_pkg.trial import square
 
     assert 4 == square(2)
+    assert 4 == square(-2)
+    assert 12.25 == square(3.5)
+    assert 2 == round(square(sqrt(2)), 5)
 
 def test_factorial():
     """Tests the factorial function."""
